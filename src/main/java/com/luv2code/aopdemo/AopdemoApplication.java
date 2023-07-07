8040c89936402fb5531798e7822a4acfb6c22473
Zuzanna Stolc <zuzanna.stolc03@gmail.com>
2023-07-07 19:21:39 +0200

    Added @After and @Around advices

--- src/main/java/com/luv2code/aopdemo/AopdemoApplication.java
+++ src/main/java/com/luv2code/aopdemo/AopdemoApplication.java
@@ -2,6 +2,7 @@ package com.luv2code.aopdemo;
 
 import com.luv2code.aopdemo.dao.AccountDAO;
 import com.luv2code.aopdemo.dao.MembershipDAO;
+import com.luv2code.aopdemo.service.TrafficFortuneService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,12 +17,39 @@ public class AopdemoApplication {
 		SpringApplication.run(AopdemoApplication.class, args);
 	}
 	@Bean
-	public CommandLineRunner commandLineRunner(AccountDAO theAccountDAO, MembershipDAO theMembershipDAO){
+	public CommandLineRunner commandLineRunner(AccountDAO theAccountDAO, MembershipDAO theMembershipDAO, TrafficFortuneService theTrafficFortuneService){
 		return runner -> {
 //			demoTheBeforeAdvice(theAccountDAO, theMembershipDAO);
 //			demoTheAfterReturningAdvice(theAccountDAO);
-			demoTheAfterThrowingAdvice(theAccountDAO);
+//			demoTheAfterThrowingAdvice(theAccountDAO);
+//			demoTheAfterAdvice(theAccountDAO);
+			demoTheAroundAdvice(theTrafficFortuneService);
 		};
+	}
+
+	private void demoTheAroundAdvice(TrafficFortuneService theTrafficFortuneService) {
+		System.out.println("\n\nMain program: demoTheAroundAdvice");
+		System.out.println("Calling getFortune()");
+		System.out.println("My fortune is: " + theTrafficFortuneService.getFortune());
+		System.out.println("Finished");
+	}
+
+	private void demoTheAfterAdvice(AccountDAO theAccountDAO) {
+		// call method to find the accounts
+		List<Account> theAccounts = null;
+		try {
+			// add a boolean flag to simulate exception
+			boolean tripWire = false;
+			theAccounts = theAccountDAO.findAccounts(tripWire);
+		}catch (Exception exc){
+			System.out.println("\n\nMain program: .. caught exception: " + exc);
+		}
+
+		// display the accounts
+		System.out.println("\n\nMain program: demoTheAfterAdvice");
+		System.out.println("-----");
+		System.out.println(theAccounts);
+		System.out.println("\n");
 	}
 
 	private void demoTheAfterThrowingAdvice(AccountDAO theAccountDAO) {
--- src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,6 +2,7 @@ package com.luv2code.aopdemo.aspect;
 
 import com.luv2code.aopdemo.Account;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
@@ -13,10 +14,34 @@ import java.util.List;
 @Component
 @Order(2)
 public class MyDemoLoggingAspect {
+    @Around("execution(* com.luv2code.aopdemo.service.*.getFortune(..))")
+    public Object aroundGetFortune(
+            ProceedingJoinPoint theProceedingJoinPoint) throws Throwable{
+        // print out method we are advising on
+        String method = theProceedingJoinPoint.getSignature().toShortString();
+        System.out.println("\n=====>>> Executing @Around on method: " + method);
+        // get begin timestamp
+        long begin = System.currentTimeMillis();
+        // now, let's execute the method
+        Object result = theProceedingJoinPoint.proceed();
+        // get end timestamp
+        long end = System.currentTimeMillis();
+        // compute duration and display it
+        long duration = end - begin;
+        System.out.println("\n=====> Duration: " + duration / 1000.0 + " seconds");
+        return result;
+    }
+    @After("execution(* com.luv2code.aopdemo.dao.AccountDAO.findAccounts(..))")
+    public void afterFinallyFindAccountsAdvice(JoinPoint theJoinPoint) {
+        // print out which method we are advising on
+        String method = theJoinPoint.getSignature().toShortString();
+        System.out.println("\n=====>>> Executing @After (finally) on method: " + method);
+    }
+
     @AfterThrowing(
             pointcut = "execution(* com.luv2code.aopdemo.dao.AccountDAO.findAccounts(..))",
             throwing = "theExc")
-    public void afterThrowingFindAccountsAdvice(JoinPoint theJoinPoint, Throwable theExc){
+    public void afterThrowingFindAccountsAdvice(JoinPoint theJoinPoint, Throwable theExc) {
         // print out which method we are advising on
         String method = theJoinPoint.getSignature().toShortString();
         System.out.println("\n=====>>> Executing @AfterThrowing on method: " + method);
@@ -24,11 +49,12 @@ public class MyDemoLoggingAspect {
         System.out.println("\n=====>>> The exception is: " + theExc);
 
     }
+
     // add a new advice for @AfterReturning on the findAccounts method
     @AfterReturning(
             pointcut = "execution(* com.luv2code.aopdemo.dao.AccountDAO.findAccounts(..))",
             returning = "result")
-    public void afterReturningFindAccountsAdvice(JoinPoint theJoinPoint, List<Account> result){
+    public void afterReturningFindAccountsAdvice(JoinPoint theJoinPoint, List<Account> result) {
         // print out which method we are advising on
         String method = theJoinPoint.getSignature().toShortString();
         System.out.println("\n=====>>> Executing @AfterReturning on method: " + method);
@@ -43,7 +69,7 @@ public class MyDemoLoggingAspect {
 
     private void convertAccountNamesToUpperCase(List<Account> result) {
         // loop through accounts
-        for (Account tempAccount: result){
+        for (Account tempAccount : result) {
             // get uppercase version of name
             String theUpperName = tempAccount.getName().toUpperCase();
             // update the name on the account
@@ -52,7 +78,7 @@ public class MyDemoLoggingAspect {
     }
 
     @Before("com.luv2code.aopdemo.aspect.LuvEopExpressions.forDaoPackageNoGetterSetter()")
-    public void beforeAddAccountAdvice(JoinPoint theJoinPoint){
+    public void beforeAddAccountAdvice(JoinPoint theJoinPoint) {
         // display the method signature
         MethodSignature methodSignature = (MethodSignature) theJoinPoint.getSignature();
         System.out.println("Method: " + methodSignature);
@@ -60,13 +86,13 @@ public class MyDemoLoggingAspect {
         // get args
         Object[] args = theJoinPoint.getArgs();
         // loop through args
-        for(Object tempArg : args){
+        for (Object tempArg : args) {
             System.out.println(tempArg);
-            if(tempArg instanceof Account){
+            if (tempArg instanceof Account) {
                 // downcast and print Account specific stuff
                 Account theAccount = (Account) tempArg;
-                System.out.println("account name: "+ theAccount.getName());
-                System.out.println("account level: "+ theAccount.getLevel());
+                System.out.println("account name: " + theAccount.getName());
+                System.out.println("account level: " + theAccount.getLevel());
             }
         }
         System.out.println("\n=====>>> Executing @Before advice on method");
